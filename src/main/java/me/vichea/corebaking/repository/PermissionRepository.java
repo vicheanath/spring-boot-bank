291b6eb0865527742b1c9f82d26bd9bfc675c153
Vichea <48352653+vicheanath@users.noreply.github.com>
2023-12-30 10:04:00 -0600

    feat: add IAM servies

--- src/main/java/me/vichea/corebaking/repository/PermissionRepository.java
+++ src/main/java/me/vichea/corebaking/repository/PermissionRepository.java
@@ -4,4 +4,5 @@ import me.vichea.corebaking.entity.Permission;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PermissionRepository extends JpaRepository<Permission, Long> {
+    Permission findByCode(String code);
 }
--- src/main/java/me/vichea/corebaking/service/impl/IAMServiceImpl.java
+++ src/main/java/me/vichea/corebaking/service/impl/IAMServiceImpl.java
@@ -4,10 +4,15 @@ import lombok.AllArgsConstructor;
 import me.vichea.corebaking.entity.Permission;
 import me.vichea.corebaking.entity.Role;
 import me.vichea.corebaking.entity.User;
+import me.vichea.corebaking.repository.PermissionRepository;
 import me.vichea.corebaking.repository.RoleRepository;
 import me.vichea.corebaking.repository.UserRepository;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 
 @Service
@@ -17,6 +22,7 @@ public class IAMServiceImpl implements IAMService{
     private final UserRepository userRepository;
     private final AuthenticationManager authenticationManager;
     private final RoleRepository roleRepository;
+    private final PermissionRepository permissionRepository;
 
     @Override
     public User createUser(User userDetails) {
@@ -26,49 +32,74 @@ public class IAMServiceImpl implements IAMService{
     @Override
     public void assignRole(User user, String roleCode) {
         Role role = roleRepository.findByCode(roleCode);
-        if (role != null) {
-            user.getRoles().add(role);
-            userRepository.save(user);
-        } else {
-            throw new RuntimeException("Role not found");
-        }
+        Optional.ofNullable(role)
+                .ifPresentOrElse(
+                        r -> {
+                            user.getRoles().add(r);
+                            userRepository.save(user);
+                        },
+                        () -> {
+                            throw new RuntimeException("Role not found");
+                        }
+                );
     }
 
     @Override
     public void removeRole(User user, String roleCode) {
         Role role = roleRepository.findByCode(roleCode);
-        if (role != null) {
-            user.getRoles().remove(role);
-            userRepository.save(user);
-        } else {
-            throw new RuntimeException("Role not found");
-        }
+        Optional.ofNullable(role)
+                .ifPresentOrElse(
+                        r -> {
+                            user.getRoles().remove(r);
+                            userRepository.save(user);
+                        },
+                        () -> {
+                            throw new RuntimeException("Role not found");
+                        }
+                );
     }
 
     @Override
     public void assignPermission(String roleCode, String permissionCode) {
         Role role = roleRepository.findByCode(roleCode);
-        Permission permission = role.getPermissions().stream().filter(p -> p.getCode().equals(permissionCode)).findFirst().orElse(null);
-        if (role != null) {
-            role.getPermissions().add(permission);
-            roleRepository.save(role);
-        } else {
-            throw new RuntimeException("Role not found");
-        }
+        Permission permission = permissionRepository.findByCode(permissionCode);
+        Optional.ofNullable(role)
+                .ifPresentOrElse(
+                        r -> {
+                            r.getPermissions().add(permission);
+                            roleRepository.save(r);
+                        },
+                        () -> {
+                            throw new RuntimeException("Role not found");
+                        }
+                );
+
     }
 
     @Override
-    public void removePermission(String role, String permission) {
-
+    public void removePermission(String roleCode, String permissionCode) {
+        Role role = roleRepository.findByCode(roleCode);
+        Permission permission = permissionRepository.findByCode(permissionCode);
+        Optional.ofNullable(role)
+                .ifPresentOrElse(
+                        r -> {
+                            r.getPermissions().remove(permission);
+                            roleRepository.save(r);
+                        },
+                        () -> {
+                            throw new RuntimeException("Role not found");
+                        }
+                );
     }
 
     @Override
     public boolean authenticateUser(String username, String password) {
-       return false;
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        return authentication.isAuthenticated();
     }
 
     @Override
-    public boolean authorize(User user, String permission) {
-        return false;
+    public boolean authorize(User user, String permissionCode) {
+        return user.getRoles().stream().anyMatch(role -> role.getPermissions().stream().anyMatch(permission -> permission.getCode().equals(permissionCode)));
     }
 }
